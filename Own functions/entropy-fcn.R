574155aa8e731954ed1bd16d41a673dceb8535e0
Avic3nna <omarelnahhas1337@gmail.com>
2021-09-28 23:13:44 +0300

    Entropy for feature selection update

--- Own functions/entropy-fcn.R
+++ Own functions/entropy-fcn.R
@@ -19,11 +19,17 @@ rm(list = ls())
 
 entropy = function(p){
   #requires sum of prob. to be equal to 1
+  
+  # freq = table(no_label_data)/length(no_label_data)
+  # 
+  # vec = as.data.frame()
+  # 
+  
   if (sum(p) != 1){
     return(NaN)
   }
   E = -sum(p*log(p), na.rm=TRUE)
-  E = E/log(length(p)) #scale entropy by log(# of classes)
+  E = E/log2(length(p)) #scale entropy by log(# of classes)
   return(E)
 }
 
@@ -42,9 +48,77 @@ entropy = function(p){
 # so that results then do fall between 0 and 1,
 
 # f.e. 4 classes with their probabilities of occuring in this cluster
-total_prob = c(0, 0, 0, 1)
 
 
-print(entropy(total_prob))
+################### ENTROPY
 
 
+set.seed(1337)
+
+root_path = "G:/My Drive/1. EIT Digital master/Estland/Semester 1/Data mining/data-mining-iti8730"
+
+setwd(root_path)
+
+#source("./Own functions/minkowsky-dist.R")
+load("./practice_3/Data/JGdata.RData")
+
+no_label_data = x[,1:2]
+
+k_dims = 2
+
+phi_regions = 10
+
+m_grid_ranges = 1:phi_regions**k_dims
+
+#p_i = faction of data points in grid region i
+
+range_d1 = range(no_label_data[,1]) #x
+range_d2 = range(no_label_data[,2]) #y
+
+start_x = min(range_d1)
+end_x = max(range_d1)
+
+start_y = min(range_d2)
+end_y = max(range_d2)
+
+total_x = abs(end_x - start_x)
+total_y = abs(end_y - start_y)
+
+grid_cell_x = total_x/phi_regions
+grid_cell_y = total_y/phi_regions
+
+min_x = start_x
+min_y = start_y
+
+points_in_cell = array(NA, phi_regions**k_dims)
+for(i in m_grid_ranges){ 
+  #100 grids, every 10 you start at 0
+  
+  if(i %in% seq(11,91, by=10)){
+    min_y = min_y + grid_cell_y
+    min_x = start_x
+  }
+
+  max_x = min_x + grid_cell_x
+  max_y = min_y + grid_cell_y
+
+  
+
+  #loop through all data points, check if they are in grid
+  counter = 0
+  for(j in seq(along=1:nrow(no_label_data))){
+    x = no_label_data[j,1]
+    y = no_label_data[j,2]
+    
+    if(x < max_x && x >= min_x){
+      if(y < max_y && y >= min_y){
+        counter = counter + 1
+      }
+    }
+  }
+  min_x = min_x + grid_cell_x
+  points_in_cell[i] = counter
+}
+
+probability = points_in_cell/nrow(no_label_data)
+print(entropy(probability))
