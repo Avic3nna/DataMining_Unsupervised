844379bb319b51b52c882ae0d3221a7850d05955
Avic3nna <omarelnahhas1337@gmail.com>
2021-10-02 18:20:43 +0300

    Applied and tested feature selection

--- Own functions/entropy-fcn.R
+++ Own functions/entropy-fcn.R
@@ -6,7 +6,7 @@
 # behaviour.
 # for every cluster has a proportion of points with a certain label (p_i), 
 # compute:
-rm(list = ls())
+
 
 # entropy_binary = function(p){
 #   if (min(p) < 0 || sum(p) <= 0 || sum(p) > 1){
@@ -17,7 +17,7 @@ rm(list = ls())
 #   return(E)
 # }
 
-entropy = function(p){
+entropy_calc = function(p){
   #requires sum of prob. to be equal to 1
   
   # freq = table(no_label_data)/length(no_label_data)
@@ -28,7 +28,7 @@ entropy = function(p){
   if (sum(p) != 1){
     return(NaN)
   }
-  E = -sum((p*log(p)), na.rm=TRUE) # + (1-p)*log(1-p) ?
+  E = -sum((p*log(p))+ (1-p)*log(1-p), na.rm=TRUE) #  ?
   #E = E/log(length(p)) #scale entropy by log(# of classes)
   return(E)
 }
@@ -60,77 +60,78 @@ root_path = "G:/My Drive/1. EIT Digital master/Estland/Semester 1/Data mining/da
 setwd(root_path)
 
 #source("./Own functions/minkowsky-dist.R")
-load("./practice_3/Data/JGData.RData")
+#load("./practice_3/Data/JGData.RData")
 
-no_label_data = x[,1:2]
-
-plot(no_label_data[,1], no_label_data[,2])
-
-k_dims = 2
-
-phi_regions = 10
-
-m_grid_ranges = 1:phi_regions**k_dims
-
-#p_i = faction of data points in grid region i
-
-range_d1 = range(no_label_data[,1]) #x
-range_d2 = range(no_label_data[,2]) #y
-
-cell_margin = 0.1
-
-start_x = min(range_d1) - cell_margin
-end_x = max(range_d1) + cell_margin
-
-start_y = min(range_d2) - cell_margin
-end_y = max(range_d2) + cell_margin
-
-total_x = abs(end_x - start_x)
-total_y = abs(end_y - start_y)
-
-grid_cell_x = total_x/phi_regions
-grid_cell_y = total_y/phi_regions
-
-min_x = start_x
-min_y = start_y
-
-points_in_cell = array(NA, phi_regions**k_dims)
-for(i in m_grid_ranges){ 
-  #100 grids, every 10 you start at 0
+entropy_2d = function(x, phi_regions=10,k_dims=2)
+{
+  no_label_data = x[,1:2]
   
-  if(i %in% seq(phi_regions+1,((phi_regions**k_dims)+1)-phi_regions, by=phi_regions)){
-    min_y = min_y + grid_cell_y
-    min_x = start_x
-  }
-
-  max_x = min_x + grid_cell_x
-  max_y = min_y + grid_cell_y
-
+  # plot(no_label_data[,1], no_label_data[,2])
   
-  # print(c(min_x,max_x))
-  # print(c(min_y, max_y))
-  # print('')
-  #loop through all data points, check if they are in grid
-  counter = 0
-  for(j in seq(along=1:nrow(no_label_data))){
-    x = no_label_data[j,1]
-    y = no_label_data[j,2]
+  # k_dims = 2
+  # 
+  # phi_regions = 10
+  
+  m_grid_ranges = 1:phi_regions**k_dims
+  
+  #p_i = faction of data points in grid region i
+  
+  range_d1 = range(no_label_data[,1]) #x
+  range_d2 = range(no_label_data[,2]) #y
+  
+  cell_margin = 0.1
+  
+  start_x = min(range_d1) - cell_margin
+  end_x = max(range_d1) + cell_margin
+  
+  start_y = min(range_d2) - cell_margin
+  end_y = max(range_d2) + cell_margin
+  
+  total_x = abs(end_x - start_x)
+  total_y = abs(end_y - start_y)
+  
+  grid_cell_x = total_x/phi_regions
+  grid_cell_y = total_y/phi_regions
+  
+  min_x = start_x
+  min_y = start_y
+  
+  points_in_cell = array(NA, phi_regions**k_dims)
+  for(i in m_grid_ranges){ 
+    #100 grids, every 10 you start at 0
     
-    if(x <= max_x && x >= min_x){
-      if(y <= max_y && y >= min_y){
-        counter = counter + 1
+    if(i %in% seq(phi_regions+1,((phi_regions**k_dims)+1)-phi_regions, by=phi_regions)){
+      min_y = min_y + grid_cell_y
+      min_x = start_x
+    }
+    
+    max_x = min_x + grid_cell_x
+    max_y = min_y + grid_cell_y
+    
+    
+    # print(c(min_x,max_x))
+    # print(c(min_y, max_y))
+    # print('')
+    #loop through all data points, check if they are in grid
+    counter = 0
+    for(j in seq(along=1:nrow(no_label_data))){
+      x = no_label_data[j,1]
+      y = no_label_data[j,2]
+      
+      if(x <= max_x && x >= min_x){
+        if(y <= max_y && y >= min_y){
+          counter = counter + 1
+        }
       }
     }
+    min_x = min_x + grid_cell_x
+    points_in_cell[i] = counter
   }
-  min_x = min_x + grid_cell_x
-  points_in_cell[i] = counter
+  sum(points_in_cell)
+  
+  probability = points_in_cell/nrow(no_label_data)
+  sum(probability)
+  output = entropy_calc(probability)
+  return(output)
 }
-sum(points_in_cell)
-probability = points_in_cell/nrow(no_label_data)
-sum(probability)
-print(entropy(probability))
 
-
-library(entropy)
-
-entropy::entropy(points_in_cell)
---
+++ Own functions/feature_selection.R
@@ -0,0 +1,67 @@
+rm(list=ls())
+set.seed(1337)
+
+root_path = "G:/My Drive/1. EIT Digital master/Estland/Semester 1/Data mining/data-mining-iti8730"
+
+setwd(root_path)
+
+
+source("./Own functions/silhouette-coeff-fcn.R")
+source("./Own functions/entropy-fcn.R")
+source("./Own functions/hopkins-stat-fcn.R")
+load("./practice_3/Data/3gaussiandata.RData")
+
+# library(rgl)
+# plot3d(x[,1], x[,2], x[,3], col=x[,4])
+# 
+# x11()
+# plot(x[,1], x[,2])
+
+
+feat_select = c(1,2,3,4) #4 is labels
+all_feat_scoeff = silhouette_coeff(x[,feat_select])
+
+
+print(all_feat_scoeff)
+
+
+#without Z
+feat_select = c(1,2,4) 
+not_z_scoeff = silhouette_coeff(x[,feat_select])
+not_z_entr = entropy_2d(x[,feat_select])
+not_z_hopk = hopkins_stat(x[,feat_select])
+
+
+print(not_z_scoeff)
+print(not_z_entr)
+print(not_z_hopk)
+
+#without Y
+feat_select = c(1,3,4)
+not_y_scoeff = silhouette_coeff(x[,feat_select])
+not_y_entr = entropy_2d(x[,feat_select])
+not_y_hopk = hopkins_stat(x[,feat_select])
+
+print(not_y_scoeff)
+print(not_y_entr)
+print(not_y_hopk)
+
+#without X
+feat_select = c(2,3,4)
+not_x_scoeff = silhouette_coeff(x[,feat_select])
+not_x_entr = entropy_2d(x[,feat_select])
+not_x_hopk = hopkins_stat(x[,feat_select])
+
+print(not_x_scoeff)
+print(not_x_entr)
+print(not_x_hopk)
+
+
+# load("./practice_3/Data/JGData.RData")
+# print(entropy_2d(x))
+
+feat_select = c(1,3,4)
+library(factoextra)
+# Compute Hopkins statistic for iris dataset
+res <- get_clust_tendency(x[,feat_select], n = 150, graph = FALSE)
+res$hopkins_stat
